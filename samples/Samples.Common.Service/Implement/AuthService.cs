bbce67a92076281fc10267ed2b0fea2d292e16fb
Xiaoxing Gao <Xiaoxing.Gao@britishcouncil.org>
2022-03-07 15:03:56 +0800

    feat: add AuthService

---
+++ samples/Samples.Common.Service/Implement/AuthService.cs
@@ -0,0 +1,30 @@
+﻿using Common.VNextFramework.EntityFramework;
+using Samples.Common.Data.Entitys;
+using Samples.Common.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samples.Common.Service.Implement
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IEFAsyncRepository<User> _userRepository;
+
+        public AuthService(IEFAsyncRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<bool> Auth()
+        {
+            var userSpec = Specification<User>.GetSpecification();
+            userSpec.AddInclude(x => x.Email);
+            userSpec.AddPredicate(x => x.UserName == "xxx");
+            var user = await _userRepository.GetSingleAsync(userSpec);
+            return true;
+        }
+    }
+}
